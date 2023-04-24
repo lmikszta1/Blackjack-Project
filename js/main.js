7595f13a1664ee2928569ae0effbd228705face7
lmikszta1 <lmikszta@gmail.com>
2023-04-24 15:57:46 -0400

    deck now renders on the page face-down and only showing the top card minor changes to styling adds some functions that have yet to be used

---
+++ js/main.js
@@ -0,0 +1,119 @@
+/*----- constants -----*/
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+
+const originalDeck = buildOriginalDeck();
+
+/*----- state variables -----*/
+let shuffledDeck;
+let numOfChips;
+let playerHand;
+let dealerHand;
+let betAmount;
+let dealtCard;
+
+/*----- cached elements  -----*/
+const deckContainer = document.getElementById('deck-container');
+const chipDisplayArea = document.getElementById('chips');
+const playerHandContainer = document.getElementById('player-container')
+const dealerHandContainer = document.getElementById('dealer-container')
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+function init(){
+    numOfChips = 5000;
+    playerHand = [];
+    dealerHand = [];
+
+    render()
+}
+init()
+
+function render() {
+    renderShuffledDeck()
+    renderChips()
+}
+
+// function to build an unshuffled deck of cards
+function buildOriginalDeck() {
+    const deck = [];
+    // Use nested forEach to generate card objects
+    suits.forEach(function(suit) {
+        ranks.forEach(function(rank) {
+            deck.push({
+                // The 'face' property maps to the library's CSS classes for cards
+                face: `${suit}${rank}`,
+                // Setting the 'value' property for game of blackjack, not war
+                value: Number(rank) || (rank === 'A' ? 11 : 10)
+            });
+        });
+    });
+    return deck;
+}
+
+// function to render the shuffled deck
+function renderShuffledDeck(){
+    shuffledDeck = getNewShuffledDeck();
+    renderDeckInContainer(shuffledDeck, deckContainer);
+}
+
+function renderChips(){
+    chipDisplayArea.innerHTML = `${numOfChips} Chips Remaining`;
+}
+
+// function to shuffle the deck
+function getNewShuffledDeck() {
+    // Create a copy of the originalDeck (leave originalDeck untouched!)
+    const tempDeck = [...originalDeck];
+    const newShuffledDeck = [];
+    while (tempDeck.length) {
+        // Get a random index for a card still in the tempDeck
+        const rndIdx = Math.floor(Math.random() * tempDeck.length);
+        // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
+        newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+    }
+    return newShuffledDeck;
+}
+
+// function to render a deck
+function renderDeckInContainer(deck, container) {
+    container.innerHTML = '';
+    // Let's build the top card as a string of HTML
+    let cardHtml = `<div class="card back-red xlarge ${deck[0].face}"></div>`;
+
+    container.innerHTML = cardHtml;
+}
+
+function dealCard(hand){
+    dealtCard = shuffledDeck.splice(0, 1)[0];
+    hand.push(dealtCard);
+}
+
+function renderHandInContainer(hand, container){
+    container.innerHTML = '';
+
+    let cardsHtml = '';
+    hand.forEach(function(card) {
+        cardsHtml += `<div class="card ${card.face}"></div>`;
+    })
+    container.innerHTML = cardsHtml;
+}
+
+function handleBet(){
+    betAmount = Number(document.getElementById('bet').value);
+    numOfChips -= betAmount;
+    renderChips();
+    let count = 0
+    while(count <= 4){
+        if(count % 2 === 0){
+            dealCard(playerHand);
+        }else{
+            dealCard(dealerHand);
+        }
+    }
+    renderHandInContainer(playerHand, playerHandContainer);
+    renderHandInContainer(dealerHand, dealerHandContainer);
+}
+
